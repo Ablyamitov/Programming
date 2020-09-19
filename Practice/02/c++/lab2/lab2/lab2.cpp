00539bc55f697b5a644122ffb0896582668df351
unknown <ablyamitov2003@mail.ru>
2020-09-19 11:35:03 +0300

    добавлено 1.5 задания

---
+++ Practice/02/c++/lab2/lab2/lab2.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a=3, b=3.14;
+	double c=3, d=3.14;
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "d = " << d;
+	return 0;
+
+
+   ;
+}
+
