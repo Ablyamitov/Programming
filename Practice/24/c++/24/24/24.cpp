2c6a7a03e88f84d5fd241b76f31b047eb80e916f
unknown <ablyamitov2003@mail.ru>
2020-11-20 22:20:59 +0300

    finaly

--- Practice/24/c++/24/24/24.cpp
+++ Practice/24/c++/24/24/24.cpp
@@ -2,24 +2,25 @@
 #include"json.h"
 #include<iostream>
 #include<iomanip>
+#include<vector>
 using nlohmann::json;
 using namespace std;
 int main()
 {
-	int complete_work = 0;
+	int complete_work ;
+	vector <int>all_userId;
 	ifstream in("in.json");
 	json inin;
 	in >> inin;
 	ofstream out("out.json");
 	json outout = json::array();
 	size_t size_inin = inin.size();
-	int count = 1;
-	for (int numsize = 0;numsize < size_inin;numsize++) {
-		if (inin[numsize]["userId"] == count) {
-			count++;
-		}
+	int maxId;
+	for (int i = 0; i < size_inin;i++) {
+		all_userId.push_back(inin[i]["userId"]);
 	}
-	for (int different_id = 1;different_id <= count;different_id++) {
+	maxId = *max_element(all_userId.begin(), all_userId.end());
+	for (int different_id = 1;different_id <= maxId;different_id++) {
 		complete_work = 0;
 		for (int allwork = 0; allwork <= size_inin;allwork++) {
 			if (inin[allwork]["userId"] == different_id && inin[allwork]["completed"])
--- Practice/24/python/_24.py
+++ Practice/24/python/_24.py
@@ -1,17 +1,16 @@
 import json
-complete_work = 0
 with open('in.json', 'r') as file:
 	inin = json.load(file)
 outout=[]
+all_userId=[]
 size_inin = len(inin)
-count = 1
-for numsize in range (size_inin):
-	if (inin[numsize]["userId"] == count):
-		count+=1
-for different_id in range (1,count+1):
+for i in range (size_inin):
+	all_userId.append(inin[i]["userId"])
+max_userId=max(all_userId)
+for different_id in range (1,max_userId+1):
 	completed_work=0
 	for allwork in range (size_inin):
-		if (inin[allwork]["userId"] == different_id and inin[allwork]["completed"]):
+		if (inin[allwork]["userId"] == different_id and inin[allwork]["completed"] ):
 			completed_work+=1
 	if(completed_work>0):
 				outout.append(
