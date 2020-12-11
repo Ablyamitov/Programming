b7e8b932e07c68c3f0ca7eb9b3240fcbe738b435
unknown <ablyamitov2003@mail.ru>
2020-12-11 12:27:57 +0300

    28 on c++

---
+++ Practice/28/c++/28/28/28.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include<cmath>
+
+bool checknumber(int a, int b) {
+    if (a > 1)
+    {
+        if (a % b == 0)
+            return true;
+    }
+    return false;
+}
+
+void print_factorization(unsigned int n){
+    int count;
+    bool checkall = true;
+    bool checkCount;
+    int Simple = 2;
+    while (checkall)
+    {
+        count = 0;
+        checkCount = checknumber(n,Simple);
+        while (checkCount)
+        {
+            count++;
+            n = n / Simple;
+            checkCount = checknumber(n, Simple);
+        }
+        if (count > 0)
+        {
+            std::cout << Simple;
+            if (count > 1)
+                std::cout << '^' << count;
+            if (n > 1)
+               std:: cout << '*';
+        }
+        Simple++;
+        if (n <= 1) 
+            checkall = false;
+    }
+}
+
+int main(){
+    int n;
+    std::cin >> n;
+    print_factorization(n);
+}
